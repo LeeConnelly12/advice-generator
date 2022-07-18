4084660af44e0c37bc535d949b6e0f7d3d2634da
Lee Connelly <lee@leeconnelly.dev>
2022-07-18 05:17:59 +0100

    Add Manrope font and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./index.html', './main.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Manrope', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        slate: {
+          400: 'hsl(193, 38%, 86%)',
+          700: 'hsl(217, 19%, 38%)',
+          800: 'hsl(217, 19%, 24%)',
+          900: 'hsl(218, 23%, 16%)',
+        },
+        green: {
+          400: 'hsl(150, 100%, 66%)',
+        },
+      },
+    },
   },
   plugins: [],
 }
