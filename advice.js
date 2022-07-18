72ab388c3ab59d70f686fa1ccbcf1549618fc943
Lee Connelly <lee@leeconnelly.dev>
2022-07-18 06:26:49 +0100

    Fix undefined advice errors

--- advice.js
+++ advice.js
@@ -1,9 +1,16 @@
 export default () => ({
   advice: {},
+  loading: false,
+
   init() {
     this.getAdvice()
   },
+
   async getAdvice() {
+    this.loading = true
+
     this.advice = await (await fetch('https://api.adviceslip.com/advice')).json()
+
+    this.loading = false
   },
 })
